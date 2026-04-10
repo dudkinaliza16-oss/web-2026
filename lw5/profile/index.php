670ec6a23dbae3b247f493b3042bdb10dd3bae2b
elizaveta-d <dudkina.liza16@gmail.com>
2026-04-10 08:47:53 +0300

    позиционирование в странице profile

--- lw5/profile/index.php
+++ lw5/profile/index.php
@@ -6,7 +6,7 @@
         <link href="../css/style_profile.css" rel="stylesheet">
     </head>
     <body>
-        <div>
+        <div class="main">
             <nav class="menu-bar">
                 <a href="#" class="menu-bar__button" title="Home">
                     <img src="../item/menu_item_home.png" width="40" height="40" alt="Home">
@@ -18,32 +18,35 @@
                     <img src="../item/menu_item_add.png" width="40" height="40" alt="Add Post">
                 </a>
             </nav>
-            <div class="profile">
-                  <img src="../images/avatar/big_avatar_vanya.png" width="123" height="123" alt="Avatar">
-                  <h1 class="profile__name">Ваня Денисов</h1>
-                  <p class="profile__description">Привет! Я системный аналитик в ACME :) Тут моя жизнь только для самых классных!</p>
-                  <div class="profile__count-post">
-                      <img src="../item/item_image.png" width="16" height="16" alt="Img">
-                      <p class="count-post__text">43 поста</p>
-                  </div>
+            <div>
+                <div class="profile">
+                    <img src="../images/avatar/big_avatar_vanya.png" width="123" height="123" alt="Avatar">
+                    <h1 class="profile__name">Ваня Денисов</h1>
+                    <p class="profile__description">Привет! Я системный аналитик в ACME :) Тут моя жизнь только для самых классных!</p>
+                    <div class="profile__count-post">
+                        <img src="../item/item_image.png" width="16" height="16" alt="Img">
+                        <p class="count-post__text">43 поста</p>
+                    </div>
+                </div>
+                <div class="profile-photos">
+                    <img src="../images/post_photo/snow_street_and_man.png" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/house_adekalon.png" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/cake.png" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/students.png" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/rokets_coffee.png" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/book.png" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/field.jpg" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/night_and_people.jpg" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/flower.jpg" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/forest.jpg" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/pot.jpg" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/sandwich.jpg" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/trees.jpg" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/two_person.jpg" width="322" height="322" alt="Photo">
+                    <img src="../images/post_photo/sunset.jpg" width="322" height="322" alt="Photo">
+                </div>
             </div>
-            <div class="profile-photos">
-                <img src="../images/post_photo/snow_street_and_man.png" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/house_adekalon.png" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/cake.png" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/students.png" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/rokets_coffee.png" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/book.png" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/field.jpg" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/night_and_people.jpg" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/flower.jpg" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/forest.jpg" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/pot.jpg" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/sandwich.jpg" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/trees.jpg" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/two_person.jpg" width="322" height="322" alt="Photo">
-                <img src="../images/post_photo/sunset.jpg" width="322" height="322" alt="Photo">
-            </div>
+
         </div>
     </body>
 </html>
